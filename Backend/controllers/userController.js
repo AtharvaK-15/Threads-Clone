1226fd77cf5713424193bd1bd56f3b4da2ed4b79
atharva <atharvak.8023@gmail.com>
2023-10-23 09:34:17 +0530

    controllers

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import e from "express";
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 const signupUser = async (req,res)=>{
     try {
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema({
     },
     profilePic:{
         type:String,
-        default:[]
+        default:""
     },
     followers:{
         type:[String],
