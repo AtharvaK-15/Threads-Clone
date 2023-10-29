bc99fa840ff03b9e0149db88637706fa05cda37a
atharva <atharvak.8023@gmail.com>
2023-10-29 15:09:15 +0530

    getfeed

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -108,4 +108,19 @@ const replytoPost = async (req, res) => {
     }
 };
 
-export {getPosts,createPost, deletePost, likeUnlikePost, replytoPost};
+const getFeed = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({message:"User not found!"});
+
+        const following = user.following;
+        const posts = await Post.find({postedBy:{$in:following}}).sort({createdAt:-1});
+        res.status(200).json({posts});
+    } catch (error) {
+        res.status(500).json({message:error.message});
+        console.log("Error in getFeed: ",error.message);
+    }
+};
+
+export {getPosts,createPost, deletePost, likeUnlikePost, replytoPost, getFeed};
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import {getPosts,createPost, deletePost, likeUnlikePost, replytoPost} from "../controllers/postController.js";
+import {getPosts,createPost, deletePost, likeUnlikePost, replytoPost, getFeed} from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.post("/create", protectRoute ,createPost); //create a post
 router.delete("/:id",protectRoute ,deletePost); //delete a post
 router.post("/like/:id",protectRoute, likeUnlikePost);
 router.post("/reply/:id",protectRoute, replytoPost);
+router.get("/feed/:id", protectRoute, getFeed);
 
 export default router;
