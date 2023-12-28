d1ee42f312f1724962447ac30b6fd33d34feedf8
atharva <atharvak.8023@gmail.com>
2023-12-28 13:38:00 +0530

    update profile

--- Frontend/src/pages/UpdateProfile.jsx
+++ Frontend/src/pages/UpdateProfile.jsx
@@ -1,6 +1,62 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  HStack,
+  Avatar,
+  AvatarBadge,
+  IconButton,
+  Center,
+}
+from '@chakra-ui/react'
+import { SmallCloseIcon } from '@chakra-ui/icons'
+
 const UpdateProfile = () => {
   return (
-    <div>UpdateProfile</div>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack
+        spacing={4}
+        w={'full'}
+        maxW={'md'}
+        bg={useColorModeValue('white', 'gray.700')}
+        rounded={'xl'}
+        boxShadow={'lg'}
+        p={6}
+        my={12}
+      >
+        <Heading lineHeight={1.1} fontSize={{base:'2xl', sm:'3xl'}}>
+          Update Profile
+        </Heading>
+        <FormControl id="username">
+          <FormLabel>User Icon</FormLabel>
+          <Stack direction={['column','row']} spacing={6}>
+            <Center>
+              <Avatar size={'xl'} src={'https://bit.ly/broken-link'} >
+                <AvatarBadge as={IconButton}
+                size={'sm'}
+                rounded={'full'}
+                top='-10px'
+                colorScheme='red'
+                aria-label='Remove Image'
+                icon={<SmallCloseIcon />}
+                />
+              </Avatar>
+            </Center>
+          </Stack>
+        </FormControl>
+
+      </Stack>
+
+    </Flex>
   )
 }
 
